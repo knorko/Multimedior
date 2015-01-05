e92b711c2291cb45ce14e91dd25e9dbb39b510a6
knorko <riklef.gesinn@gmail.com>
2015-01-05 18:17:28 +0100

    Signed-off-by: knorko <riklef.gesinn@gmail.com>

--- System/boid.cpp
+++ System/boid.cpp
@@ -68,36 +68,38 @@ void boid::Update(){
     vector2 v1;
     vector2 v2;
     vector2 v3;
-    vector2 v4(getCanvasWidth()/2 , getCanvasHeight()/2);
-    vector2 center= v4;
+    vector2 v4;
+    vector2 center = position;
 
     //Rule1: move to local center of mass ( center becomes average of surrounding boids)
+    vector2 lc = vector2();
     for(int i = 0; i < 3; i++){
-        center = center + neighbours[i].position2;
-//        //Rule2: Avoidance : if distance to next boid smaller than threshold T boid changes course.
-//        if ((neighbours[i].position2 - position).getSqrMagnitude()<100){
-//            center = center - (neighbours[i].position2 - position);
-//        }
-            qDebug() << "center: "<< center.getX()<<" "<<center.getY()<<" Neighbour: "<<neighbours[i].position2.getX() <<" "<<neighbours[i].position2.getY();
+        lc = lc + neighbours[i].position2;
     }
-    center = center / 3;
-    v1 = center - position;
+    lc = lc / 3;
+
+    v1 = lc - position;
 
     //Rule2: Avoidance : if distance to next boid smaller than threshold T boid changes course.
-    center = center - center;
+    center = vector2();
     for(int i = 0; i < 3; i++){
-        if ((neighbours[i].position2 - position).getSqrMagnitude()<1000){
+        if ((neighbours[i].position2 - position).getSqrMagnitude()<getSize() * getSize()){
             center = center - (neighbours[i].position2 - position);
         }
     }
     v2 = center;
+    lastVel = v2;
     //Rule3: Match velocity to surrounding Boids
     for(int i = 0; i < 3; i++){
         v3 = v3 + neighbours[i].velocity2;
     }
     v3 = v3/3;
-    v3 = (v3 - position)/4;
-    velocity = velocity + v1 + v2 + v3;
+    //v3 = (v3 - position)/4;
+    vector2 mp = getMousePosition();
+        if(!(mp == vector2(0, 0)))
+            if((mp - position).getSqrMagnitude() > 7500)
+            v4 = mp - position;
 
+    velocity = velocity + v1/100 + v2*50 + v3/50 + v4/2500;
 
 }
