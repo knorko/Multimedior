dd117c41217f14027e1843ec7f012a05ef9d9edf
TheSniperFan <thesniperfan@gmail.com>
2015-01-07 22:52:34 +0100

    Proper documentation for boids and predators.
    Signed-off-by: TheSniperFan <thesniperfan@gmail.com>

--- System/boid.h
+++ System/boid.h
@@ -6,9 +6,13 @@
 /**
  * @brief The boid class contains all boid logic.
  *
- * Boids have certain behavior patterns. They flock, while avoiding getting to close
- * to each other, they flee, when a Predator gets too close and (optionally) follow a
- * global target.
+ * Boids have certain behavior patterns. The first behavior is flocking. They strive towards
+ * center of mass, of their neighbors. The second behavior is avoidance, which makes them
+ * try to keep a certain distance to them. The next pattern is (velocity) matching. They adapt
+ * their movement speed to speed of their neighbors. Further than that, the boids can either
+ * strive towards or flee from a global goal (mouse), if one is set. On top of that, they're
+ * pushed try to steer away from the borders of the canvas. The last behavior pattern is the
+ * fleeing from every nearby #Predator.
  */
 class Boid : public BoidHelper {
 public:
--- System/predator.h
+++ System/predator.h
@@ -6,7 +6,9 @@
 /**
  * @brief The Predator class contains the predator logic
  *
- * Predators sweep the area randomly, until they find a Boid, which they then pursue.
+ * Predators sweep the area randomly until they find a #Boid, which they then pursue.
+ * Besides that, they match their velocity to the velocity of surrounding boids. Lastly
+ * they steer away from the boundaries.
  */
 class Predator : public BoidHelper {
 public:
